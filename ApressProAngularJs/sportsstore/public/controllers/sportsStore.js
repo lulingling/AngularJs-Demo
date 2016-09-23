66cc876c62dd9f1b1064590bf1f234cd1ed8f08a
lulingling <lovelyfevdy@gmail.com>
2016-09-23 11:25:11 +0800

    Apress.Pro.AngularJs ebook chapter7 demo(sportsstore: navigation and checkout)

--- ApressProAngularJs/sportsstore/public/controllers/sportsStore.js
+++ ApressProAngularJs/sportsstore/public/controllers/sportsStore.js
@@ -1,31 +1,14 @@
 angular.module("sportsStore")
-    .controller("sportsStoreCtrl", function ($scope) {
-        $scope.data = {
-            products: [
-                {
-                    name: "Products #1",
-                    description: "A product",
-                    category: "Category #1",
-                    price: 100
-                },
-                {
-                    name: "Products #2",
-                    description: "A product",
-                    category: "Category #2",
-                    price: 110
-                },
-                {
-                    name: "Products #3",
-                    description: "A product",
-                    category: "Category #3",
-                    price: 210
-                },
-                {
-                    name: "Products #4",
-                    description: "A product",
-                    category: "Category #4",
-                    price: 202
-                }
-            ]
-        }
+    .constant("dataUrl", "http://localhost:5500/products")
+    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+        $scope.data = {};
+
+        $http.get(dataUrl)
+            .success(function (data) {
+                console.log(data);
+                $scope.data.products = data;
+            })
+            .error(function (error) {
+                $scope.data.error = error;
+            })
     });
