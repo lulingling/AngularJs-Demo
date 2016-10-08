17d53f2780dd389947bdcc3bf8cdfbb898827d1f
lulingling <lovelyfevdy@gmail.com>
2016-10-08 11:50:23 +0800

    ProAngularJs demo -- (21 demo)

--- ApressProAngularJs/chap21/products/public/products.js
+++ ApressProAngularJs/chap21/products/public/products.js
@@ -1,50 +1,98 @@
-angular.module("exampleApp", [])
-    .controller("defaultCtrl", function ($scope) {
+angular.module("exampleApp", ["increment", "ngResource"])
+    .constant("baseUrl", "http://localhost:5500/products/")
+    .controller("defaultCtrl", function ($scope, $http, $resource, baseUrl) {
+
         $scope.displayMode = "list";
         $scope.currentProduct = null;
 
+        $scope.productsResource = $resource(baseUrl + ":id", {id: "@id"});
+
         $scope.listProducts = function () {
-            $scope.products = [
-                {id: 0, name: "Dummy1", category: "Test", price: 1.25},
-                {id: 1, name: "Dummy2", category: "Test", price: 2.45},
-                {id: 2, name: "Dummy3", category: "Test", price: 4.25}
-            ]
+            /*$http.get(baseUrl).success(function (data) {
+             $scope.products = data;
+             });*/
+
+            $scope.products = $scope.productsResource.query();
         };
 
         $scope.deleteProduct = function (product) {
-            $scope.products.splice($scope.products.indexOf(product), 1);
-        };
+            /*var rep = {
+             method: "DELETE",
+             url: baseUrl + product.id
+             };
+             $http(rep).success(function () {
+             $scope.products.splice($scope.products.indexOf(product), 1);
+             });*/
 
-        $scope.createProduct = function (product) {
-            $scope.products.push(product);
+            product.$delete().then(function () {
+                $scope.products.splice($scope.products.indexOf(product), 1);
+            });
             $scope.displayMode = "list";
         };
 
-        $scope.updateProduct = function (product) {
-            for (var i = 0; i < $scope.products.length; i++) {
-                if ($scope.products[i].id == product.id) {
-                    $scope.products[i] = product;
-                    break;
-                }
-            }
+        $scope.createProduct = function (product) {
+            /*$http.post(baseUrl, product).success(function (newProduct) {
+             $scope.products.push(newProduct);
+             $scope.displayMode = "list";
+             });*/
 
+            new $scope.productsResource(product).$save().then(function (newProduct) {
+                $scope.products.push(newProduct);
+                $scope.displayMode = "list";
+            })
+        };
+
+        $scope.updateProduct = function (product) {
+            /*var rep = {
+             url: baseUrl + product.id,
+             method: "PUT",
+             data: product
+             };
+
+             $http(rep).success(function (modifiedProduct) {
+             for (var i = 0; i < $scope.products.length; i++) {
+             if ($scope.products[i].id == modifiedProduct.id) {
+             $scope.products[i] = product;
+             break;
+             }
+             }
+             $scope.displayMode = "list";
+             });*/
+
+            product.$save();
             $scope.displayMode = "list";
         };
 
         $scope.editOrCreateProduct = function (product) {
-            $scope.currentProduct = product ? angular.copy(product) : {};
+            /*$scope.currentProduct =
+             product ? angular.copy(product) : {};
+             $scope.displayMode = "edit";*/
+
+            $scope.currentProduct = product ? product : {};
             $scope.displayMode = "edit";
         };
 
         $scope.saveEdit = function (product) {
+            /*if (angular.isDefined(product.id)) {
+             $scope.updateProduct(product);
+             } else {
+             $scope.createProduct(product);
+             }*/
+
             if (angular.isDefined(product.id)) {
-                $scope.updateProduct(product)
+                $scope.updateProduct(product);
             } else {
                 $scope.createProduct(product);
             }
         };
 
-        $scope.cancelEdit = function (product) {
+        $scope.cancelEdit = function () {
+            /*$scope.currentProduct = {};
+             $scope.displayMode = "list";*/
+
+            if ($scope.currentProduct && $scope.currentProduct.$get) {
+                $scope.currentProduct.$get();
+            }
             $scope.currentProduct = {};
             $scope.displayMode = "list";
         };
